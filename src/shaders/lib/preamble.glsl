486e8e9dc6164675338a37b88d78da647a245350
Misaki Kasumi <misakikasumi@outlook.com>
2026-05-21 14:03:20 -0700

    chore: fmt shaders

--- src/shaders/lib/preamble.glsl
+++ src/shaders/lib/preamble.glsl
@@ -3,8 +3,8 @@
 #extension GL_EXT_integer_dot_product : require
 #extension GL_EXT_shader_16bit_storage : require
 #extension GL_EXT_shader_explicit_arithmetic_types_float16 : require
-#extension GL_EXT_shader_explicit_arithmetic_types_int16: require
-#extension GL_EXT_shader_explicit_arithmetic_types_int8: require
+#extension GL_EXT_shader_explicit_arithmetic_types_int16 : require
+#extension GL_EXT_shader_explicit_arithmetic_types_int8 : require
 #extension GL_KHR_shader_subgroup_arithmetic : require
 #extension GL_KHR_shader_subgroup_basic : require
 #extension GL_KHR_shader_subgroup_shuffle : require
